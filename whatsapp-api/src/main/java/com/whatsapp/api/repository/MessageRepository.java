b77ae74f81c8190f17cd2766d770c90be6d47322
Your Name <skprjapati028@gmail.com>
2025-08-07 22:07:40 +0530

    updated

--- whatsapp-api/src/main/java/com/whatsapp/api/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.whatsapp.api.repository;
-
-import com.whatsapp.api.model.Message;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface MessageRepository extends ReactiveMongoRepository<Message, String> {
-}
